317dffdfa1b0810f73785e905f2d50f8f83c5e1b
DenislavKirchev <denislav_delon@abv.bg>
2020-11-11 19:22:13 +0000

    .

--- src/exercise7/NumberCounter.java
+++ src/exercise7/NumberCounter.java
@@ -28,7 +28,7 @@ public class NumberCounter {
 
     public static int countNumberOccurrences(int number, int[] numbers) {
         int count = 0;
-        for(int i = 0; i <= numbers.length; i++){
+        for(int i = 0; i <= numbers.length-1; i++){
           if(numbers[i]==number){
             count++;
           }
